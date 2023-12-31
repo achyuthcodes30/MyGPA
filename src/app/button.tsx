72e67c090165ac3ed91c663979c14c2286f7be22
Achyuth Sosale <achyuthyogesh0@gmail.com>
2023-12-31 15:06:58 +0530

    Adds: Added button animations

--- src/app/button.tsx
+++ src/app/button.tsx
@@ -4,8 +4,31 @@ import { lato } from "./fonts";
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { stagger, useAnimate } from "framer-motion";
 
 export default function Button() {
+  const [cgpascope, cgpaanimate] = useAnimate();
+  const [sgpascope, sgpaanimate] = useAnimate();
+  const cgpa = "Find my CGPA";
+  const sgpa = "Find my SGPA";
+
+  const onCgpaButtonClick = () => {
+    cgpaanimate([
+      [".cgpaletter", { y: -27 }, { duration: 0.4, delay: stagger(0.05) }],
+      [".cgpa-button", { scale: 0.2 }, { duration: 0.1, at: "<" }],
+      [".cgpa-button", { scale: 1 }, { duration: 0.05 }],
+      [".cgpaletter", { y: 0 }, { duration: 0.0000001, at: 0.05 }],
+    ]);
+  };
+
+  const onSgpaButtonClick = () => {
+    sgpaanimate([
+      [".sgpaletter", { y: -27 }, { duration: 0.4, delay: stagger(0.05) }],
+      [".sgpa-button", { scale: 0.2 }, { duration: 0.1, at: "<" }],
+      [".sgpa-button", { scale: 1 }, { duration: 0.05 }],
+      [".sgpaletter", { y: 0 }, { duration: 0.0000001, at: 0.05 }],
+    ]);
+  };
   useEffect(() => {
     AOS.init({
       easing: "ease",
@@ -14,25 +37,43 @@ export default function Button() {
   }, []);
   return (
     <>
-      <span className="mx-2">
+      <span ref={cgpascope} className="mx-2">
         <button
-          className={`cool-button relative flex h-12 w-40 items-center justify-center  border-2 border-sky-600 p-4 shadow-2xl`}
+          className={`cgpa-button flex h-12 w-40 items-center justify-center border-2 border-sky-600 p-4 shadow-2xl transition-colors hover:bg-sky-50`}
           data-aos="fade-right"
           data-aos-duration="1400"
+          onClick={onCgpaButtonClick}
         >
-          <span className={`text-cyan-600 ${lato.className} text-lg`}>
-            Find my CGPA
+          <span className="relative flex items-center justify-center overflow-hidden bg-transparent">
+            {cgpa.split("").map((letter, index) => (
+              <span
+                data-letter={letter}
+                key={` ${letter + "-" + index}`}
+                className={`text-sky-600 ${lato.className} cgpaletter relative inline-block text-lg after:absolute after:left-0 after:top-full after:text-lg after:content-[attr(data-letter)]`}
+              >
+                {letter === " " ? "\u00A0" : letter}
+              </span>
+            ))}
           </span>
         </button>
       </span>
-      <span className="mx-2">
+      <span ref={sgpascope} className="mx-2">
         <button
-          className={`cool-button  relative flex h-12 w-40 items-center justify-center border-2  border-sky-600 p-4 shadow-2xl`}
+          className={`sgpa-button flex h-12 w-40 items-center justify-center border-2 border-sky-600  p-4 shadow-2xl transition-colors hover:bg-sky-50`}
           data-aos="fade-left"
           data-aos-duration="1400"
+          onClick={onSgpaButtonClick}
         >
-          <span className={`text-cyan-600 ${lato.className} text-lg`}>
-            Find my SGPA
+          <span className="relative flex items-center justify-center overflow-hidden bg-transparent">
+            {sgpa.split("").map((letter, index) => (
+              <span
+                data-letter={letter}
+                key={` ${letter + "-" + index}`}
+                className={`text-sky-600 ${lato.className} sgpaletter relative inline-block text-lg after:absolute after:left-0 after:top-full after:text-lg after:content-[attr(data-letter)]`}
+              >
+                {letter === " " ? "\u00A0" : letter}
+              </span>
+            ))}
           </span>
         </button>
       </span>
