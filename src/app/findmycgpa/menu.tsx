26914162f5b72875da3ca0cf752e51db0737500d
Achyuth Sosale <achyuthyogesh0@gmail.com>
2024-01-04 00:37:30 +0530

    touch-ups

--- src/app/findmycgpa/menu.tsx
+++ src/app/findmycgpa/menu.tsx
@@ -4,7 +4,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "../../styles/menu.css";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { spectral } from "../fonts";
 
 type MenuProps = {
@@ -27,7 +26,7 @@ function Menu(props: MenuProps) {
       }
     }
   }
-  const router = useRouter();
+
   const aniref = useRef(null);
   const [rotation, setRotation] = useState(false);
   const rotateOnClick = () => {
@@ -44,7 +43,7 @@ function Menu(props: MenuProps) {
       <div
         data-aos="fade-down-right"
         data-aos-duration="1400"
-        className="absolute z-[99999]"
+        className="absolute z-[999999]"
       >
         <div
           className={` absolute h-auto w-auto cursor-pointer rounded-br-full bg-white pb-4 pl-1 pr-4 pt-3 lg:pb-6 lg:pl-2 lg:pr-6 lg:pt-4`}
@@ -61,32 +60,31 @@ function Menu(props: MenuProps) {
           </div>
         </div>
       </div>
-      <Link href="../" className="absolute z-[99999999]">
-        <span
-          className={`absolute left-[4.5vw] top-[4vh] z-[99999999] -rotate-[48deg] text-xl text-black lg:left-[6vw] lg:top-[15vh] lg:rotate-0 lg:text-4xl
+
+      <span
+        className={`absolute left-[4.5vw] top-[4vh] z-[199] -rotate-[48deg] text-xl text-black lg:left-[6vw] lg:top-[15vh] lg:rotate-0 lg:text-4xl
          ${spectral.className} ${
            rotation
-             ? "opacity-100 transition-all duration-[1800ms]"
-             : " opacity-0 transition-all duration-300"
+             ? "opacity-100 transition-all delay-[200ms] duration-[1600ms]"
+             : " opacity-0 transition-all duration-500"
          }`}
-          style={{ fontWeight: 800 }}
-        >
-          {link1}
-        </span>
-      </Link>
-      <Link href={`../findmy${linkertext}`}>
-        <span
-          className={`absolute left-[8vw] top-[10vh] z-[999] -rotate-[48deg] text-xl text-black lg:left-[15vw] lg:top-[50vh] lg:rotate-0 lg:text-4xl
+        style={{ fontWeight: 800 }}
+      >
+        {rotation && <Link href="../">{link1}</Link>}
+      </span>
+
+      <span
+        className={`absolute left-[8vw] top-[10vh] z-[199] -rotate-[48deg] text-xl text-black lg:left-[15vw] lg:top-[50vh] lg:rotate-0 lg:text-4xl
          ${spectral.className} ${
            rotation
              ? " opacity-100 transition-all duration-[2000ms] "
-             : "duration-[0.0001ms]transition-all opacity-0"
+             : "opacity-0 transition-all duration-300"
          }`}
-          style={{ fontWeight: 800 }}
-        >
-          {link2}
-        </span>
-      </Link>
+        style={{ fontWeight: 800 }}
+      >
+        {rotation && <Link href={`../findmy${linkertext}`}> {link2}</Link>}
+      </span>
+
       <nav
         id="nav"
         className={` absolute inset-0 overflow-hidden transition-all duration-1000 ${
@@ -94,16 +92,9 @@ function Menu(props: MenuProps) {
         }`}
       >
         <ul>
-          <li className="shape-circle circle-one curser-pointer">
-            <Link
-              href={`../findmy${linkertext}`}
-              className="absolute z-[20] h-full w-full"
-            ></Link>
-          </li>
+          <li className="shape-circle circle-one curser-pointer"></li>
 
-          <li className="shape-circle circle-one curser-pointer">
-            <Link href="../" className="absolute z-[20] h-full w-full"></Link>
-          </li>
+          <li className="shape-circle circle-one curser-pointer"></li>
         </ul>
       </nav>
     </div>
